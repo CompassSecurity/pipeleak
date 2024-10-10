7bc1a713275e892eca256b2adf5e9c3ae008dbfd
jf <jan.friedli@compass-security.com>
2024-10-10 11:32:06 +0200

    missing comma

--- src/pipeleak/scanner/queue.go
+++ src/pipeleak/scanner/queue.go
@@ -283,7 +283,7 @@ func DownloadEnvArtifact(cookieVal string, gitlabUrl string, prjectPath string, 
 }
 
 // https://docs.gitlab.com/ee/ci/caching/#common-use-cases-for-caches
-var skippableDirectoryNames = []string{"node_modules", ".yarn", ".yarn-cache" ".npm", "venv", "vendor", ".go/pkg/mod/"}
+var skippableDirectoryNames = []string{"node_modules", ".yarn", ".yarn-cache", ".npm", "venv", "vendor", ".go/pkg/mod/"}
 
 func handleArchiveArtifact(archivefileName string, content []byte, jobWebUrl string) {
 	for _, skipKeyword := range skippableDirectoryNames {
