962b37c3a721d40a8ecdb8e0820808c06c1b8873
jf <jan.friedli@compass-security.com>
2024-10-17 15:39:37 +0200

    defer arter error check

--- src/pipeleak/nist/nist.go
+++ src/pipeleak/nist/nist.go
@@ -9,10 +9,10 @@ import (
 func FetchVulns(version string) (string, error) {
 	client := helper.GetNonVerifyingHTTPClient()
 	res, err := client.Get("https://services.nvd.nist.gov/rest/json/cves/2.0?cpeName=cpe:2.3:a:gitlab:gitlab:" + version + ":*:*:*:*:*:*:*")
-	defer res.Body.Close()
 	if err != nil {
 		return "{}", err
 	}
+	defer res.Body.Close()
 
 	if res.StatusCode == 200 {
 		resData, err := io.ReadAll(res.Body)
