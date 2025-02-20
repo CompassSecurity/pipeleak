25bb7d32f88002d58d342663b5b65c313cf49066
frjcomp <107982661+frjcomp@users.noreply.github.com>
2025-02-20 07:32:51 +0000

    fixed bug and added minimum access level options

--- src/pipeleak/cmd/enum.go
+++ src/pipeleak/cmd/enum.go
@@ -4,8 +4,10 @@ import (
 	"github.com/CompassSecurity/pipeleak/helper"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
-	"gitlab.com/gitlab-org/api/client-go"
+	gitlab "gitlab.com/gitlab-org/api/client-go"
 )
+
+var minAccessLevel int
 
 func NewEnumCmd() *cobra.Command {
 	enumCmd := &cobra.Command{
@@ -25,6 +27,8 @@ func NewEnumCmd() *cobra.Command {
 		log.Fatal().Msg("Unable to require token flag")
 	}
 	enumCmd.MarkFlagsRequiredTogether("gitlab", "token")
+
+	enumCmd.PersistentFlags().IntVarP(&minAccessLevel, "level", "l", int(gitlab.GuestPermissions), "Minimum repo access level. See https://docs.gitlab.com/api/access_requests/#valid-access-levels for integer values")
 
 	enumCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "Verbose logging")
 	return enumCmd
@@ -46,51 +50,54 @@ func Enum(cmd *cobra.Command, args []string) {
 	log.Info().Msg("Enumerating User")
 	log.Warn().Str("username", user.Username).Str("name", user.Name).Str("email", user.Email).Bool("admin", user.IsAdmin).Bool("bot", user.Bot).Msg("Current user")
 
-	log.Info().Msg("Enumerating repositories with at least guest access")
+	log.Info().Int("minAccessLevel", minAccessLevel).Msg("Enumerating repositories with minimum access level")
 	projectOpts := &gitlab.ListProjectsOptions{
 		ListOptions: gitlab.ListOptions{
 			PerPage: 100,
 			Page:    1,
 		},
-		MinAccessLevel: gitlab.Ptr(gitlab.GuestPermissions),
+		MinAccessLevel: gitlab.Ptr(gitlab.AccessLevelValue(minAccessLevel)),
 		OrderBy:        gitlab.Ptr("last_activity_at"),
 	}
 
-	projects, resp, err := git.Projects.ListProjects(projectOpts)
-	if err != nil {
-		log.Error().Stack().Err(err).Msg("Failed fetching projects")
-	}
-
-	for _, project := range projects {
-		logLine := log.Warn().Str("project", project.WebURL).Str("name", project.NameWithNamespace).Bool("publicJobs", project.PublicJobs)
-
-		if len(project.Description) > 0 {
-			logLine.Str("description", project.Description)
+	for {
+		projects, resp, err := git.Projects.ListProjects(projectOpts)
+		if err != nil {
+			log.Error().Stack().Err(err).Msg("Failed fetching projects")
 		}
 
-		if project.Permissions.ProjectAccess != nil {
-			logLine.Int("projectAccessLevel", int(project.Permissions.ProjectAccess.AccessLevel))
-		}
+		for _, project := range projects {
+			logLine := log.Warn().Str("project", project.WebURL).Str("name", project.NameWithNamespace).Bool("publicJobs", project.PublicJobs)
 
-		if project.Permissions.GroupAccess != nil {
-			logLine.Int("groupAcessLevel", int(project.Permissions.GroupAccess.AccessLevel))
-		}
+			if len(project.Description) > 0 {
+				logLine.Str("description", project.Description)
+			}
 
-		logLine.Msg("Repo")
+			if project.Permissions.ProjectAccess != nil {
+				logLine.Int("projectAccessLevel", int(project.Permissions.ProjectAccess.AccessLevel))
+			}
+
+			if project.Permissions.GroupAccess != nil {
+				logLine.Int("groupAcessLevel", int(project.Permissions.GroupAccess.AccessLevel))
+			}
+
+			logLine.Msg("Repo")
+		}
 
 		if resp.NextPage == 0 {
 			break
 		}
+
 		projectOpts.Page = resp.NextPage
 	}
 
-	log.Info().Msg("Enumerating groups with at least guest access")
+	log.Info().Int("minAccessLevel", minAccessLevel).Msg("Enumerating groups with minimum access level")
 	listGroupsOpts := &gitlab.ListGroupsOptions{
 		ListOptions: gitlab.ListOptions{
 			PerPage: 100,
 			Page:    1,
 		},
-		MinAccessLevel: gitlab.Ptr(gitlab.GuestPermissions),
+		MinAccessLevel: gitlab.Ptr(gitlab.AccessLevelValue(minAccessLevel)),
 		TopLevelOnly:   gitlab.Ptr(false),
 	}
 
