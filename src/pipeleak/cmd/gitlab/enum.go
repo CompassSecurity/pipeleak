7a2c38cbf0da8dedc7c9d55462fdce1c868c5324
frjcomp <107982661+frjcomp@users.noreply.github.com>
2025-04-29 13:51:07 +0200

    Refactor Enum CMD (#164)
    * Use new GitLab endpoints for enumeration

--- src/pipeleak/cmd/gitlab/enum.go
+++ src/pipeleak/cmd/gitlab/enum.go
@@ -1,10 +1,17 @@
 package gitlab
 
 import (
+	"net/url"
+	"path"
+	"strconv"
+	"strings"
+	"time"
+
 	"github.com/CompassSecurity/pipeleak/helper"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	gitlab "gitlab.com/gitlab-org/api/client-go"
+	"resty.dev/v3"
 )
 
 var minAccessLevel int
@@ -50,76 +57,140 @@ func Enum(cmd *cobra.Command, args []string) {
 	log.Info().Msg("Enumerating User")
 	log.Warn().Str("username", user.Username).Str("name", user.Name).Str("email", user.Email).Bool("admin", user.IsAdmin).Bool("bot", user.Bot).Msg("Current user")
 
-	log.Info().Int("minAccessLevel", minAccessLevel).Msg("Enumerating repositories with minimum access level")
-	projectOpts := &gitlab.ListProjectsOptions{
-		ListOptions: gitlab.ListOptions{
-			PerPage: 100,
-			Page:    1,
-		},
-		MinAccessLevel: gitlab.Ptr(gitlab.AccessLevelValue(minAccessLevel)),
-		OrderBy:        gitlab.Ptr("last_activity_at"),
+	log.Info().Msg("Enumerating Access Token")
+	client := *resty.New().SetRedirectPolicy(resty.FlexibleRedirectPolicy(5))
+	enumCurrentToken(client, gitlabUrl, gitlabApiToken)
+
+	log.Info().Msg("Enumerating Projects and Groups")
+	page := 1
+	for page != -1 {
+		page = listTokenAssociations(client, gitlabUrl, gitlabApiToken, minAccessLevel, page)
 	}
 
-	for {
-		projects, resp, err := git.Projects.ListProjects(projectOpts)
-		if err != nil {
-			log.Error().Stack().Err(err).Msg("Failed fetching projects")
-		}
+	log.Info().Msg("Done")
+}
 
-		for _, project := range projects {
-			logLine := log.Warn().Str("project", project.WebURL).Str("name", project.NameWithNamespace).Bool("publicJobs", project.PublicJobs)
+type TokenAssociations struct {
+	Groups []struct {
+		ID             int         `json:"id"`
+		WebURL         string      `json:"web_url"`
+		Name           string      `json:"name"`
+		ParentID       interface{} `json:"parent_id"`
+		OrganizationID int         `json:"organization_id"`
+		AccessLevels   int         `json:"access_levels"`
+		Visibility     string      `json:"visibility"`
+	} `json:"groups"`
+	Projects []struct {
+		ID                int       `json:"id"`
+		Description       string    `json:"description"`
+		Name              string    `json:"name"`
+		NameWithNamespace string    `json:"name_with_namespace"`
+		Path              string    `json:"path"`
+		PathWithNamespace string    `json:"path_with_namespace"`
+		CreatedAt         time.Time `json:"created_at"`
+		AccessLevels      struct {
+			ProjectAccessLevel int `json:"project_access_level"`
+			GroupAccessLevel   int `json:"group_access_level"`
+		} `json:"access_levels"`
+		Visibility string `json:"visibility"`
+		WebURL     string `json:"web_url"`
+		Namespace  struct {
+			ID        int         `json:"id"`
+			Name      string      `json:"name"`
+			Path      string      `json:"path"`
+			Kind      string      `json:"kind"`
+			FullPath  string      `json:"full_path"`
+			ParentID  interface{} `json:"parent_id"`
+			AvatarURL string      `json:"avatar_url"`
+			WebURL    string      `json:"web_url"`
+		} `json:"namespace"`
+	} `json:"projects"`
+}
 
-			if len(project.Description) > 0 {
-				logLine.Str("description", project.Description)
-			}
+type SelfToken struct {
+	ID          int       `json:"id"`
+	Name        string    `json:"name"`
+	Revoked     bool      `json:"revoked"`
+	CreatedAt   time.Time `json:"created_at"`
+	Description string    `json:"description"`
+	Scopes      []string  `json:"scopes"`
+	UserID      int       `json:"user_id"`
+	LastUsedAt  time.Time `json:"last_used_at"`
+	Active      bool      `json:"active"`
+	ExpiresAt   string    `json:"expires_at"`
+	LastUsedIps []string  `json:"last_used_ips"`
+}
 
-			if project.Permissions.ProjectAccess != nil {
-				logLine.Int("projectAccessLevel", int(project.Permissions.ProjectAccess.AccessLevel))
-			}
+func enumCurrentToken(client resty.Client, baseUrl string, pat string) {
+	u, err := url.Parse(baseUrl)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Unable to parse base URL")
+	}
+	u.Path = path.Join(u.Path, "api/v4/personal_access_tokens/self")
+	currentToken := &SelfToken{}
+	res, err := client.R().
+		SetHeader("PRIVATE-TOKEN", pat).
+		SetResult(currentToken).
+		Get(u.String())
 
-			if project.Permissions.GroupAccess != nil {
-				logLine.Int("groupAcessLevel", int(project.Permissions.GroupAccess.AccessLevel))
-			}
-
-			logLine.Msg("Repo")
-		}
-
-		if resp.NextPage == 0 {
-			break
-		}
-
-		projectOpts.Page = resp.NextPage
+	if res.StatusCode() != 200 {
+		log.Error().Stack().Any("any", res.RawResponse.Body).Msg("Failed fetching token associations")
 	}
 
-	log.Info().Int("minAccessLevel", minAccessLevel).Msg("Enumerating groups with minimum access level")
-	listGroupsOpts := &gitlab.ListGroupsOptions{
-		ListOptions: gitlab.ListOptions{
-			PerPage: 100,
-			Page:    1,
-		},
-		MinAccessLevel: gitlab.Ptr(gitlab.AccessLevelValue(minAccessLevel)),
-		TopLevelOnly:   gitlab.Ptr(false),
+	if err != nil {
+		log.Error().Stack().Err(err).Msg("Failed fetching token associations")
 	}
 
-	for {
-		groups, resp, err := git.Groups.ListGroups(listGroupsOpts)
-		if err != nil {
-			log.Error().Stack().Err(err).Msg("failed listing groups")
-		}
+	log.Warn().
+		Int("id", currentToken.ID).
+		Str("name", currentToken.Name).
+		Bool("revoked", currentToken.Revoked).
+		Time("created", currentToken.CreatedAt).
+		Str("description", currentToken.Description).
+		Str("scopes", strings.Join(currentToken.Scopes, ",")).
+		Int("userId", currentToken.UserID).
+		Time("lastUsedAt", currentToken.LastUsedAt).
+		Bool("active", currentToken.Active).
+		Str("lastUsedIps", strings.Join(currentToken.LastUsedIps, ",")).
+		Msg("Current Token")
+}
 
-		for _, group := range groups {
-			logLine := log.Warn().Str("group", group.WebURL).Str("fullName", group.FullName).Str("name", group.Name).Str("visibility", string(group.Visibility))
-
-			if len(group.Description) > 0 {
-				logLine.Str("description", group.Description)
-			}
-
-			logLine.Msg("Group")
-		}
-
-		if resp.NextPage == 0 {
-			break
-		}
-		listGroupsOpts.Page = resp.NextPage
+// https://docs.gitlab.com/api/personal_access_tokens/#list-all-token-associations
+func listTokenAssociations(client resty.Client, baseUrl string, pat string, accessLevel int, page int) int {
+	u, err := url.Parse(baseUrl)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Unable to parse base URL")
 	}
+	u.Path = path.Join(u.Path, "api/v4/personal_access_tokens/self/associations")
+	resp := &TokenAssociations{}
+	res, err := client.R().
+		SetHeader("PRIVATE-TOKEN", pat).
+		SetResult(resp).
+		SetQueryParam("min_access_level", strconv.Itoa(accessLevel)).
+		SetQueryParam("per_page", "100").
+		SetQueryParam("page", strconv.Itoa(page)).
+		Get(u.String())
+
+	if res.StatusCode() != 200 {
+		log.Error().Stack().Str("Str", res.String()).Int("code", res.StatusCode()).Msg("Failed fetching token associations")
+	}
+
+	if err != nil {
+		log.Error().Stack().Err(err).Msg("Failed fetching token associations")
+	}
+
+	for _, group := range resp.Groups {
+		log.Warn().Str("group", group.WebURL).Int("accessLevel", group.AccessLevels).Str("name", group.Name).Str("visibility", string(group.Visibility)).Msg("Group")
+	}
+
+	for _, project := range resp.Projects {
+		log.Warn().Str("project", project.WebURL).Str("name", project.NameWithNamespace).Int("groupAccessLevel", project.AccessLevels.GroupAccessLevel).Int("projectAccessLevel", project.AccessLevels.ProjectAccessLevel).Msg("Project")
+	}
+
+	nextPage, err := strconv.Atoi(res.Header().Get("x-next-page"))
+	if err != nil {
+		nextPage = -1
+	}
+
+	return nextPage
 }
